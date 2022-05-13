81f87ba968f4ea849311d85d2d899e11892a5a2e
alan <sing2016@gmail.com>
2022-05-13 23:24:02 +0800

    完善钱包充值微信H5端逻辑

--- pages/wallet/recharge.vue
+++ pages/wallet/recharge.vue
@@ -32,6 +32,9 @@
 </template>
 
 <script>
+	// #ifdef H5
+	import wx from '@/common/libs/jweixin.js'
+	// #endif
 	export default {
 		data() {
 			return {
@@ -52,13 +55,56 @@
 			pay(payData){
 				let data = {
 					rechargeId:payData.id,
-					payment:payData.payment
+					payment:payData.payment,
+					// #ifdef H5
+					isH5:true,
+					// #endif
 				}
 				this.$u.api.payRecharge(data).then(res => {
-					this.wechatPay(res.data,0);
+					// #ifdef MP-WEIXIN
+						this.wechatPay(res.data,0);
+					// #endif
+					// #ifdef H5
+						this.wechatPay(res.data,0,wx);
+					// #endif
 				});
 			},
+			wx_config(){
+				let params = {url: window.location.href.split('#')[0]}
+				this.$u.api.getWechatSignPackage(params).then(res => {
+					let data = res.code === 200 ? res.data : {}
+					wx.config({
+						debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+						appId: data.appId, // 必填，公众号的唯一标识
+						timestamp: data.timestamp, // 必填，生成签名的时间戳
+						nonceStr: data.nonceStr, // 必填，生成签名的随机串
+						signature: data.signature, // 必填，签名
+						jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表
+					})
+					wx.ready(function() {
+						// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+						wx.checkJsApi({
+							jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+							success: function(res) {
+								// 以键值对的形式返回，可用的api值true，不可用为false
+								// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+							},
+						});
+					});
+					wx.error(function(res){
+					  // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+						console.log('接口验证失败',res)
+					});
+				}).catch(err => {
+					// console.log(err)
+				})
+			}
+		},
+		// #ifdef H5
+		onReady(){
+			this.wx_config();
 		}
+		// #endif
 	}
 </script>
 
