c534678b08b573a28ed65f3e8677b4ac95bd4196
alan <sing2016@gmail.com>
2022-05-17 01:05:32 +0800

    v1.0.1

--- main.js
+++ main.js
@@ -39,8 +39,8 @@ Vue.component('pdsGoodsRecommend',pdsGoodsRecommend)
 //==============================================================================================
 
 app.$mount()
-Vue.prototype.pdsVersion = 'v1.0.0'
-Vue.prototype.pdsVersionCode = '20220505'
+Vue.prototype.pdsVersion = 'v1.0.1'
+Vue.prototype.pdsVersionCode = '20220517'
 Vue.prototype.pdsSystemInfo = uni.getSystemInfoSync()
 Vue.prototype.pdsSymbol = '¥'
 
